0e68553481666fdc111d9dfd4612bdd0eabfb5c5
Desktoo <akshatkri1463@gmail.com>
2025-01-16 13:55:52 +0530

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <section className='common-padding bg-gray-300 items-center justify-center '>
+        <div className='text-gray-100 text-center'>
+            <p className='text-xl'>This is a clone project of Apple website </p><br/>
+            <p className='text-sm'>Credits : JavaScript Mastery</p>
+            <p className='text-sm'>Developed by Akshat Krishan</p>
+        </div>
+    </section>
+  )
+}
+
+export default Footer
